1a5c7fd4f60a3dd5a3bfdb7d8d67c21d9844fea9
SMY7527 <kdo7527@naver.com>
2025-12-24 16:42:03 +0900

    test

--- booknaraPrj/src/main/java/com/booknara/booknaraPrj/BooknaraPrjApplication.java
+++ booknaraPrj/src/main/java/com/booknara/booknaraPrj/BooknaraPrjApplication.java
@@ -1,4 +1,4 @@
-package com.example.booknaraPrj;
+package com.booknara.booknaraPrj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
